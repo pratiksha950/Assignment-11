4ade3c07e56a6c064efe69ec3c5592e0ce657c45
pratiksha950 <salunkepratiksha950@gmail.com>
2025-08-09 22:09:27 +0530

    javaScript added

---
+++ assignment-11.js
@@ -0,0 +1,8 @@
+function overlayModal(){
+    const overlayModal=document.getElementById("overlay");
+    overlayModal.style.display="flex";
+}
+function overlayModalClosed(){
+    const overlayModal=document.getElementById("overlay");
+    overlayModal.style.display="none";
+}
